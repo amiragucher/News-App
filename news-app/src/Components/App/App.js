cb946a5bc7a98ebaa365cf482c667f42cad68af8
amiragucher <amiragucher@gmail.com>
2022-06-15 23:28:03 +0100

    Fetching from api

--- news-app/src/Components/App/App.js
+++ news-app/src/Components/App/App.js
@@ -1,21 +1,22 @@
-
 import React, { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 
-  function App() {
-const [data, setData] = useState([]);
-useEffect(() => {
-(async ()=>{
-  // const response = await fetch (`${process.env.API_URL}?token=${process.env.API_TOKEN}&q=global`)
-  // const data = await response.json();
-  console.log(`${process.env.API_URL}?token=${process.env.API_TOKEN}&q=global`);
-})()
-}, [])
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}?token=${process.env.REACT_APP_API_TOKEN}&q=global`
+      );
+      const data = await response.json();
+      setData(data.articles);
+      console.log(data);
+    })();
+  }, []);
 
   return (
     <div className="App">
-     <h1>News App</h1>
-     
+      <h1>News App</h1>
     </div>
   );
 }
