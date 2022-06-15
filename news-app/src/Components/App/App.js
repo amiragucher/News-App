e12c0b6eb72f9b7e931e22d1bbfaaa2b2320bc47
amiragucher <amiragucher@gmail.com>
2022-06-15 23:13:45 +0100

    del

--- news-app/src/Components/App/App.js
+++ news-app/src/Components/App/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+  function App() {
+const [data, setData] = useState([]);
+useEffect(() => {
+(async ()=>{
+  // const response = await fetch (`${process.env.API_URL}?token=${process.env.API_TOKEN}&q=global`)
+  // const data = await response.json();
+  console.log(`${process.env.API_URL}?token=${process.env.API_TOKEN}&q=global`);
+})()
+}, [])
+
   return (
     <div className="App">
-     <p>New App</p>
+     <h1>News App</h1>
+     
     </div>
   );
 }
---
+++ news-app/src/Components/NewsSearchBar/NewsSearchBar.js
@@ -0,0 +1,7 @@
+export default function NewsSearchBar (){
+    return (
+        <>
+        <p>Hello are you working?</p>
+        </>
+    )
+}
