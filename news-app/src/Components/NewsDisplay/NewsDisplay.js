c39d3a7d634f02acfbcd02e93fb9aa7fb49e78ef
amiragucher <amiragucher@gmail.com>
2022-06-18 21:26:20 +0100

    Changes file name: DisplayNews

--- news-app/src/Components/NewsDisplay/NewsDisplay.js
+++ news-app/src/Components/NewsDisplay/NewsDisplay.js
@@ -1,22 +1,26 @@
-import NewsSearchBar from "../NewsSearchBar/NewsSearchBar";
-
 export default function NewsDisplay ({data}){
     return (
-        <div className="ArticlesData">
-        data.map((data) => (
-        <ol key = { data.id }>
-        title={data.title},
-        description={data.description},
-        content={data.content},
-        url={data.url},
-        image={data.image},
-        publishedAt={data.publishedAt},
-        source={data.source},
-        name={data.source.name},
-        sourceUrl={data.source.url} 
-        </ol>
-        </div>
-    );
+        <div>
+        <ol className="articles">
+    {/* {data.map((data) => {
+        return( */}
+            <li 
+        title={data.title}
+        description={data.description}
+        content={data.content}
+        url={data.url}
+        image={data.image}
+        // publishedAt={data.publishedAt}
+        source={data.source}>
+        {/* // name={data.source.name}
+        // sourceUrl={data.source.url} > */}
+
+        </li>
+        {/* ) */}
+    {/* })} */}
+    </ol>
+    </div>
+    )
    
 }
 
