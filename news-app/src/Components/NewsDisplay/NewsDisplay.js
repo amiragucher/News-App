1143e186fffe251e36e118a4b537ad2ff3d7fd90
amiragucher <amiragucher@gmail.com>
2022-06-18 20:59:40 +0100

    Display not rendering, change function

--- news-app/src/Components/NewsDisplay/NewsDisplay.js
+++ news-app/src/Components/NewsDisplay/NewsDisplay.js
@@ -2,19 +2,21 @@ import NewsSearchBar from "../NewsSearchBar/NewsSearchBar";
 
 export default function NewsDisplay ({data}){
     return (
-        <div>
-        data.map((data) => ({
+        <div className="ArticlesData">
+        data.map((data) => (
         <ol key = { data.id }>
-        title={data.title}
-        description={data.description} 
-        content={data.content}
-        url={data.url}
-        image={data.image}
-        publishedAt={data.publishedAt}
-        source={data.source}
-        name={data.source.name}S
+        title={data.title},
+        description={data.description},
+        content={data.content},
+        url={data.url},
+        image={data.image},
+        publishedAt={data.publishedAt},
+        source={data.source},
+        name={data.source.name},
         sourceUrl={data.source.url} 
         </ol>
         </div>
-    )}
+    );
+   
+}
 
