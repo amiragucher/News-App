b0cc4fd79539859ee51e1bdd6843e2bea3425cf3
amiragucher <amiragucher@gmail.com>
2022-06-19 12:35:13 +0100

    Changes added

--- news-app/src/Components/NewsDisplay/NewsDisplay.js
+++ news-app/src/Components/NewsDisplay/NewsDisplay.js
@@ -1,24 +1,28 @@
+import NewsSearchBar from "../NewsSearchBar/NewsSearchBar"
+
 export default function NewsDisplay ({data}){
     return (
         <div>
-        <ol className="articles">
-    {/* {data.map((data) => {
-        return( */}
-            <li 
-        title={data.title}
-        description={data.description}
-        content={data.content}
-        url={data.url}
-        image={data.image}
-        // publishedAt={data.publishedAt}
-        source={data.source}>
-        {/* // name={data.source.name}
-        // sourceUrl={data.source.url} > */}
-
-        </li>
-        {/* ) */}
-    {/* })} */}
-    </ol>
+        {
+        data.map((data) => {
+        return(  
+            <NewsSearchBar
+            title={data.title}
+            description={data.description}
+            content={data.content}
+            url={data.url}
+            image={data.image}
+            publishedAt={data.publishedAt}
+            source={data.source}
+        // name={data.source.name}
+        // sourceUrl={data.source.url} 
+        /> 
+           
+        
+         ) 
+     })
+     } 
+    
     </div>
     )
    
