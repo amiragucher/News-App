63a44d0c8d984630b623acf1ba40e705baf9972b
amiragucher <amiragucher@gmail.com>
2022-07-12 19:51:42 +0100

    project complete

--- news-app/src/Components/NewsDisplay/NewsItem/NewsItem.js
+++ news-app/src/Components/NewsDisplay/NewsItem/NewsItem.js
@@ -5,7 +5,9 @@ export default function NewsItem ({title, description, conttent, url, publisedAt
         <div className={Style.ArticlesContainer}>
          <h2 className={Style.ArticleTitle}>{title}</h2>
          <a href={sourceUrl}>
+            <div className={Style.ImageContainer}>
         <img className={Style.Image} src={image}  />
+        </div>
         </a>  
         </div>
         
